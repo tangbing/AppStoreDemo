935ecb66ee94aea8753d1199295f66f777256f86
Tb <tbit_144135@sina.com>
2020-10-22 00:26:05 +0800

    提交game

---
+++ MyAppStoreDemo/module/Games/Model/GameRecommandModel.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  MyAppStoreDemo
+//
+//  Created by Tb on 2020/10/21.
+//  Copyright © 2020 Tb. All rights reserved.
+//
+
+import Foundation
+
+struct GameRecommandModel {
+    let feature: String
+    let name: String
+    let desc: String
+    let coverImageName: String
+}
+
---
+++ MyAppStoreDemo/module/Games/View/Topic/GameTopicCollectionViewCell/GameTopicCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  GameTopicCollectionViewCell.swift
+//  MyAppStoreDemo
+//
+//  Created by Tb on 2020/10/21.
+//  Copyright © 2020 Tb. All rights reserved.
+//
+
+import UIKit
+
+protocol GameTopicCollectionCellDelegage: class {
+    func gameTopicCollectionCellDidClick(cell tipiccell: GameTopicCollectionViewCell)
+    
+}
+
+class GameTopicCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var lineView: UIView!
+    
+   weak var delegage: GameTopicCollectionCellDelegage?
+    
+    var model:GameTopicModel! {
+        didSet {
+            iconImageView.image = UIImage(named: model.iconImageName)
+            nameLabel.text = model.name
+            descLabel.text = model.desc
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBAction func getAction(_ sender: UIButton) {
+        delegage?.gameTopicCollectionCellDidClick(cell: self)
+    }
+
+}
